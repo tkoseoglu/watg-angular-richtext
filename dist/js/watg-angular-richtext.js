9d211dc780f25aebfd1cce23e1f744b6e35b7441
Kemal Tolga Koseoglu <tolga.k@outlook.com>
2016-10-18 14:36:33 -0600

    DIST Template url

--- dist/js/watg-angular-richtext.js
+++ dist/js/watg-angular-richtext.js
@@ -17,8 +17,8 @@
     function watgRichtext() {
         return {
             restrict: 'E',
-            templateUrl: 'src/app/directives/templates/watgRichtextEditorTemplate.html',
-            //templateUrl: 'app/directives/templates/watgRichtextEditorTemplate.html',
+            //templateUrl: 'src/app/directives/templates/watgRichtextEditorTemplate.html',
+            templateUrl: 'app/directives/templates/watgRichtextEditorTemplate.html',
             scope: {
                 id: "=",
                 richText: '=',
--- src/app/directives/watgRichtextEditorDirective.js
+++ src/app/directives/watgRichtextEditorDirective.js
@@ -8,8 +8,8 @@
     function watgRichtext() {
         return {
             restrict: 'E',
-            templateUrl: 'src/app/directives/templates/watgRichtextEditorTemplate.html',
-            //templateUrl: 'app/directives/templates/watgRichtextEditorTemplate.html',
+            //templateUrl: 'src/app/directives/templates/watgRichtextEditorTemplate.html',
+            templateUrl: 'app/directives/templates/watgRichtextEditorTemplate.html',
             scope: {
                 id: "=",
                 richText: '=',
